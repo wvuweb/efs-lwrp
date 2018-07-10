8163435839214e57e6a867fe835ec831af70ceee
Steven Spriggs <>
2018-07-10 10:15:12 -0400

    add default attributes and default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+Chef::Log.info("Installing EFS")
+efs_lwrp "install_efs" do
+  mount_dir node[:efs][:mount_dir]
+  mount_flags node[:efs][:mount_flags]
+  efs_dns node[:efs][:efs_dns]
+end
