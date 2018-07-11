8cd3051937eb58cbefe05c7773309f14c15b1fb5
Steven Spriggs <>
2018-07-11 13:43:27 -0400

    fix typo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default[:efs][:mount_dir] = '/mnt/efs'
-default[:efs][:mount_flgs] = '-t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2'
+default[:efs][:mount_flags] = '-t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2'
 default[:efs][:efs_dns] = nil
